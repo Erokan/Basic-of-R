d45e9c1261dc866491af02e2dfd33b23c32c6852
Erokan Canbazoğlu <erokan@gmail.com>
2016-04-18 10:49:07 +0300

    Explanations of qnorm and rnorm are added

--- 6-Basic Statistics in R/1-Normal Distribution.R
+++ 6-Basic Statistics in R/1-Normal Distribution.R
@@ -1,4 +1,5 @@
 # pnorm - Given a set of values it calculates the probability that a normally distributed random number will be less than that number.
+# Also called the cumulative distribution function
 pnorm(0)
 pnorm(1)
 x<-seq(-20,20,0.1)
@@ -14,6 +15,7 @@ x<-seq(-20,20,0.1)
 y<-dnorm(x)
 plot(x,y,type="l",col=2)
 
+# qnorm - Given a probability, it returns the number whose cumulative distribution matches the probability
 qnorm(0)
 qnorm(1)
 qnorm(0.5)
@@ -22,6 +24,7 @@ x<-seq(0,1,0.1)
 y<-qnorm(x)
 plot(x,y,type="l",col=2)
 
+# rnorm - Generates random numbers whose distribution is normal
 rnorm(4)
 hist(rnorm(20))
 hist(rnorm(200))
